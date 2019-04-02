76c6956cfecf05da2b80050eab5a71ead31e50d1
keith <mrgitservice@outlook.com>
2019-04-02 09:21:46 +0800

    Create memoized.js

---
+++ algorithm/memoized.js
@@ -0,0 +1,15 @@
+const memoized = (fn) => {
+    // 利用闭包的作用域来记忆结果，减少不必要的重复运算。
+    const lookupTable = {};
+    return (arg) => lookupTable[arg] || (lookupTable[arg] = fn(arg));
+}
+
+let fastFactorial = memoized((n) => {
+    if (n === 0) {
+        return 1;
+    }
+    return n * fastFactorial(n - 1);
+})
+
+var res = fastFactorial(10);
+console.log();
